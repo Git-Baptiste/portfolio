08fa58ce4f101d8f262f9c495e1c186e5db33bfc
Baptiste Mnt <baptiste.menant@gmail.com>
2022-05-03 15:55:49 +0200

    Fix du menu non affiché lors du clic

--- scripts/app.js
+++ scripts/app.js
@@ -20,11 +20,11 @@ btnNav.addEventListener('click', () => {
             }
       }else {
             if(imgBtn.src.includes('menu')){
-                  imgBtn.src = '../ressources/croix.svg';
+                  imgBtn.src = './ressources/croix.svg';
                   imgBtn.style.width = "30px";
                   imgBtn.style.height = "30px";      
             } else {
-                  imgBtn.src = '../ressources/menu.svg';
+                  imgBtn.src = './ressources/menu.svg';
                   imgBtn.style.width = "40px";
                   imgBtn.style.height = "40px";  
             }
